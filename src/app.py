91ca73f0a6b210e88e5421bfdd43dec9fc522625
Zach Shay <zach@zshay.io>
2025-02-04 05:53:02 -0500

    Modified to include a model path

--- src/app.py
+++ src/app.py
@@ -4,6 +4,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
+# needed for path creation
+import os
+
 # Load dataset
 iris = datasets.load_iris()
 x = iris.data
@@ -24,6 +27,13 @@ y_pred = clf.predict(x_test)
 # Model Accuracy, how often is the classifier correct?
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
 
+# Create the 'models' directory if it doesn't exist
+models_dir = "models"
+os.makedirs(models_dir, exist_ok=True)  # Create the directory if it doesn't exist
+
+# Construct the full path to the model file
+model_path = os.path.join(models_dir, "iris_model.pkl")
+
 # Save the trained model
-joblib.dump(clf, 'iris_model.pkl')
-print("Model saved!")
+joblib.dump(clf, model_path)
+print(f"Model saved to: {model_path}!")
