5cce17f553858e4fbb8d5ec77c02260861a6ddf3
Zach Shay <zach@zshay.io>
2025-02-04 05:40:51 -0500

    Refactored X to x

--- src/app.py
+++ src/app.py
@@ -6,20 +6,20 @@ import joblib
 
 # Load dataset
 iris = datasets.load_iris()
-X = iris.data
+x = iris.data
 y = iris.target
 
 # Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 # Create a Gaussian Classifier
 clf = RandomForestClassifier()
 
 # Train the model using the training sets
-clf.fit(X_train, y_train)
+clf.fit(x_train, y_train)
 
 # Predict the response for test dataset
-y_pred = clf.predict(X_test)
+y_pred = clf.predict(x_test)
 
 # Model Accuracy, how often is the classifier correct?
 print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
